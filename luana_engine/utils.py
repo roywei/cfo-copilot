5f6334f517e20ce7b560676376777b62e9bf040c
Lai Wei <royweilai@gmail.com>
2023-09-11 00:40:07 -0700

    fix load env

--- luana_engine/utils.py
+++ luana_engine/utils.py
@@ -10,12 +10,16 @@ def load_dotenv():
     dotenv_path = join(dirname(__file__), "../.env")
     # read each line and assign env var name and value split by '='
     print("loading env file from: ", dotenv_path)
-    for line in open(dotenv_path):
-        var = line.strip().split("=")
-        if len(var) == 2:
-            key, value = var[0].strip(), var[1].strip()
-            # set the env var
-            os.environ[key] = value
+    # check if file exists
+    if os.path.exists(dotenv_path):
+        for line in open(dotenv_path):
+            var = line.strip().split("=")
+            if len(var) == 2:
+                key, value = var[0].strip(), var[1].strip()
+                # set the env var
+                os.environ[key] = value
+    else:
+        print("WARNING: .env file not found")
 
 
 def merge_deltas(original, delta):
